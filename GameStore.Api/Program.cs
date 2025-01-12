c8bdf3bb72ffef63edffc7245c3875a0fd056ce7
vladislav-gorovenko <vladislav.gorovenko.webdev@gmail.com>
2025-01-13 00:20:58 +0400

    initial commit

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapPost("/who-is-nastya", () => "Nastya is princessa.");
 
 app.Run();
